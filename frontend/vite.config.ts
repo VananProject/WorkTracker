a56158e0d858a3ab2fd07158e12142df4adc7c14
VananProject <Supports@vananservices.in>
2025-07-14 12:13:22 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['bp.vananpicture.com'],
     port: 5173,
     host: '0.0.0.0',
     open: true
