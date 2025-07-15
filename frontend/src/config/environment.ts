0836a7a659d26af6dc447a8eb9b18b533d51f991
VananProject <Supports@vananservices.in>
2025-07-15 17:36:17 +0530

    Update environment.ts

--- frontend/src/config/environment.ts
+++ frontend/src/config/environment.ts
@@ -1,5 +1,5 @@
 const config = {
-  API_BASE_URL: (window as any).env?.REACT_APP_API_BASE_URL || 'http://3.6.23.137:5000/api',
+  API_BASE_URL: (window as any).env?.REACT_APP_API_BASE_URL || 'https://bp.backend.vananpicture.com/api',
   NODE_ENV: 'development',
 };
 
