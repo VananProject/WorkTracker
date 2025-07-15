13e55641b70feb8b49bfed98b2bf7d457c3aad6c
VananProject <Supports@vananservices.in>
2025-07-15 17:27:28 +0530

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -106,7 +106,8 @@ app.use(cors({
     'http://localhost:3000', 
     'http://localhost:5173',
     'http://127.0.0.1:3000',
-    'http://127.0.0.1:5173'
+    'http://127.0.0.1:5173',
+    'https://bp.backend.vananpicture.com'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
