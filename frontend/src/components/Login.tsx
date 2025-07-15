f769b496693abd9f36737f0e57ec2c3f7e15eea8
VananProject <Supports@vananservices.in>
2025-07-15 18:40:04 +0530

    Update Login.tsx

--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -311,9 +311,9 @@ const Login: React.FC<LoginProps> = ({ onSwitchToSignup }) => {
               )}
             </Button>
 
-            { <Divider sx={{ mb: 3, opacity: 0.6 }} /> }
+            {/* <Divider sx={{ mb: 3, opacity: 0.6 }} /> */}
 
-            { <Box sx={{ textAlign: 'center' }}>
+            {/* <Box sx={{ textAlign: 'center' }}>
               <Typography variant="body2" color="text.secondary" sx={{ mb: 1 }}>
                 New to Vanan Online Services?
               </Typography>
@@ -334,7 +334,7 @@ const Login: React.FC<LoginProps> = ({ onSwitchToSignup }) => {
               >
                 Create Corporate Account
               </Button>
-            </Box> }
+            </Box> */}
           </form>
         </Box>
 
