a8e50741872c53c3075cef67a5e6a75b4d65bdfb
VananProject <Supports@vananservices.in>
2025-07-15 15:22:10 +0530

    Update AuthContext.tsx

--- frontend/src/hooks/AuthContext.tsx
+++ frontend/src/hooks/AuthContext.tsx
@@ -119,7 +119,7 @@
 //     dispatch({ type: 'LOGIN_START' });
 
 //     try {
-//       const response = await fetch('http://3.6.23.137:5000/api/auth/signup', {
+//       const response = await fetch('http://localhost:5000/api/auth/signup', {
 //         method: 'POST',
 //         headers: {
 //           'Content-Type': 'application/json',
@@ -329,7 +329,7 @@ const signup = async (
   dispatch({ type: 'LOGIN_START' });
 
   try {
-    const response = await fetch('http://3.6.23.137:5000/api/auth/signup', {
+    const response = await fetch('https://3.6.23.137:5000/api/auth/signup', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
